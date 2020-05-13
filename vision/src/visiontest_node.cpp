f5ad41dfc25f1c82701767ebfef4387fab3b0deb
ivelso <ivelso@github.com>
2020-05-13 22:17:05 +1000

    blobdetection working on the compressed image

--- vision/src/visiontest_node.cpp
+++ vision/src/visiontest_node.cpp
@@ -1,14 +1,7 @@
 /**
     visiontest.cpp
 
-    A ROS node that searching in colour images for any beacons and tries to 
-    find its location relative to the robot.
-
-    Subscribed: camera/colour/image_raw/compressed
-    Publishes: 
-
-    Created: 2020/02/06
-    Author: Brendan Halloran
+  http://wiki.ros.org/visp_ros
 **/
 
 // Standard Library
@@ -30,168 +23,72 @@
 #include <visp3/core/vpImageConvert.h>
 #include <visp_ros/vpROSGrabber.h>
 #include <iostream>
+#include <visp3/core/vpImage.h>
+#include <visp3/detection/vpDetectorQRCode.h>
+#include <visp3/vision/vpPose.h>
+#include <visp_ros/vpROSRobot.h>
+
 // #include <visp_ros/include/visp_ros/vpROSGrabber.h>
 
 // Local Headers
 
 namespace vision
 {
-   
 
-    class visionTestNode
+  class visionTestNode
+  {
+  public:
+    // Constants
+    static constexpr double DEPTH_SCALE = 0.001;
+    vpDisplay *disp;
+    bool firstRun = true;
+    bool init_done = false;
+    vpDot2 blob;
+    vpImagePoint germ;
+    visionTestNode()
     {
-    public:
-        // Constants
-        static constexpr double DEPTH_SCALE = 0.001;
-        vpDisplay* disp;
-        bool firstRun = true;
-        bool init_done = false;
-        vpDot2 blob;
-        vpImagePoint germ;
-        visionTestNode()
-        {
-          
-            std::cout << "here " << std::endl;
-            this->subscriber_colour_image = nh.subscribe<sensor_msgs::Image>("camera/color/image_raw/", 1, &visionTestNode::callback_colour_image, this);
-            this->subscriber_depth = nh.subscribe<sensor_msgs::Image>("camera/aligned_depth_to_color/image_raw/", 1, &visionTestNode::callback_depth, this);
 
-        }
+      std::cout << "here " << std::endl;
+      //this->subscriber_colour_image = nh.subscribe<sensor_msgs::Image>("camera/color/image_raw/", 1, &visionTestNode::callback_colour_image, this);
+      //this->subscriber_depth = nh.subscribe<sensor_msgs::Image>("camera/aligned_depth_to_color/image_raw/", 1, &visionTestNode::callback_depth, this);
+    }
 
-        void callback_colour_image(sensor_msgs::ImageConstPtr const& colour_image )//sensor_msgs::ImageConstPtr const& colour_image 
-        {
-          
-            vpImage<vpRGBa> vispImage;
-           
-            vispImage = visp_bridge::toVispImageRGBa(*colour_image);
-            vpImage<unsigned char> greyImage;
-            vpImageConvert::convert(vispImage, greyImage);
+    void callback_colour_image(sensor_msgs::ImageConstPtr const &colour_image) //sensor_msgs::ImageConstPtr const& colour_image
+    {
 
-            if (firstRun){
-                //disp = new vpDisplayX();
-                disp = new vpDisplayX(greyImage, 0, 0, "camera view");
-                disp->init(greyImage);
-               // //disp->setTitle("Image processing initialisation interface");
-                firstRun= false;
-                std::cout << "first run " << std::endl;
-                //vpDisplay::display(greyImage);
-                //vpDisplay::flush(greyImage);
-            }
-            // vpDisplay::display(greyImage);
-            
-            /**
-            try{
-              if (!init_done) {
-                  std::cout << "initdone  " << init_done << std::endl;
-            disp->displayText(greyImage, vpImagePoint(10, 10), "Click in the blob to initialize the tracker", vpColor::red);
-            vpDisplay::flush(greyImage);
-            while (!init_done ){
-            std::cout << "first run " << std::endl;
-            if (vpDisplay::getClick(greyImage, germ, false)) {
-               std::cout << "init done  " << std::endl;
-                blob.setGraphics(true);
-                blob.setGraphicsThickness(2);
-                blob.initTracking(greyImage, germ);
-                init_done = true;
-                }}}
+      vpImage<vpRGBa> vispImage;
 
-             else {
-                blob.track(greyImage,germ);
-                std::cout << "blob  " << std::endl;
-            }
-            }
-            catch (...){
-                init_done=false;
-            }
-            disp->flush(greyImage);
-            std::cout << "flush  " << std::endl;
+      vispImage = visp_bridge::toVispImageRGBa(*colour_image);
+      vpImage<unsigned char> greyImage;
+      vpImageConvert::convert(vispImage, greyImage);
 
-            **/
-          
-           
-            disp->display(greyImage);
-            disp->flushDisplay();
-            
-             
+      if (firstRun)
+      {
+        //disp = new vpDisplayX();
+        disp = new vpDisplayX(greyImage, 0, 0, "camera view");
+        disp->init(greyImage);
+        // //disp->setTitle("Image processing initialisation interface");
+        firstRun = false;
+        std::cout << "first run " << std::endl;
+        //vpDisplay::display(greyImage);
+        //vpDisplay::flush(greyImage);
+      }
+      // vpDisplay::display(greyImage);
+    }
+  };
+} // namespace vision
 
-            // Get image message as an OpenCV Mat object (most functions you use for image processing will want this)
-            //cv::Mat frame = cv_bridge::toCvShare(colour_image, sensor_msgs::image_encodings::BGR8)->image;
-            // vpImage<vpRGBa> vispImage;
-            /**
-           
-            #ifdef VISP_HAVE_X11
-            vpDisplayX d(vispImage);
-            #else
-             std::cout << "No image viewer is available..." << std::endl;
-            #endif
-            // vpImageConvert::convert(&frame,vispImage);
-         **/
-            std::cout << "Image size: " << vispImage.getWidth() << " " << vispImage.getHeight() << std::endl;
-            //vpDisplay::setTitle(vispImage,"image");
-            //vpDisplay::display(vispImage);
-            // vpDisplay::flush(vispImage);
-            // cv::imshow("cv_img", frame);
-           // cv::waitKey(2);
-        }
-
-        void callback_depth(sensor_msgs::ImageConstPtr const& depth_image)
-        {
-            ROS_INFO("callback_depth()");
-
-            // Get image message as an OpenCV Mat object (most functions you use for image processing will want this)
-            cv::Mat frame = cv_bridge::toCvCopy(depth_image, sensor_msgs::image_encodings::TYPE_16UC1)->image; // Note encoding
-
-            // Find the depth of the centre pixel in metres
-            cv::Point2i centre_point(frame.cols / 2, frame.rows / 2);
-            double centre_depth = DEPTH_SCALE * static_cast<double>(frame.at<uint16_t>(centre_point));
-            ROS_INFO("centre depth: %.4f", centre_depth);
-
-            // cv::imshow("cv_depth", frame);
-            cv::waitKey(2);
-        }
-
-    private:
-        ros::Subscriber subscriber_colour_image, subscriber_depth;
-        ros::NodeHandle nh;
-   
-    };
-} // namespace ecte477
-/**
-int main(int argc, char** argv)
+int main(int argc, const char **argv)
 {
-    ROS_INFO("Starting ROS Beacon Detector module");
-    ros::init(argc, argv, "visiontest_node");
-   
-    vision::visionTestNode bd;
-    /**
-    vpImage<vpRGBa> vispImage;
-    #ifdef VISP_HAVE_X11
-    std::cout << "image" << std::endl;
-    ROS_INFO("Starting ROS");
-    vpDisplayX d(vispImage);
-    ROS_INFO("Starting ROS Beacon  module");
-    #else
-    ROS_INFO("Starting ROS  Detector module");
-    std::cout << "No image viewer is available..." << std::endl;
-    #endif
-    **/
-/***
-    ros::spin();
-    ROS_INFO("Shutting down ROS Beacon Detector module");
-
-    return 0;
-}
-**/
-
-
-
-int main(int argc, const char** argv)
-{
-  try {
+  try
+  {
     bool opt_use_camera_info = false;
-    for (int i=0; i<argc; i++) {
+    for (int i = 0; i < argc; i++)
+    {
       if (std::string(argv[i]) == "--use-camera-info")
         opt_use_camera_info = true;
-      else if (std::string(argv[i]) == "--help") {
+      else if (std::string(argv[i]) == "--help")
+      {
         std::cout << "Usage: " << argv[0]
                   << " [--use-camera-info] [--help]"
                   << std::endl;
@@ -200,68 +97,68 @@ int main(int argc, const char** argv)
     }
     std::cout << "Use camera info: " << ((opt_use_camera_info == true) ? "yes" : "no") << std::endl;
 
-    //vpImage<unsigned char> I; // Create a gray level image container
-    vpImage<vpRGBa> I; // Create a color image container
+    vpImage<unsigned char> I; // Create a gray level image container
+    //vpImage<vpRGBa> I; // Create a color image container
     vpROSGrabber g; // Create a grabber based on ROS
+    vpDot2 blobDetector;
+    blobDetector.setGraphics(true);
+    blobDetector.setGraphicsThickness(2);
+    vpImagePoint detectedPoints;
+    bool init_done = false;
 
-    g.setImageTopic("/camera/color/image_raw");
-    if (opt_use_camera_info) {
+    g.setImageTopic("/camera/color/image_raw/compressed");
+    g.setImageTransport("jpeg");
+    if (opt_use_camera_info)
+    {
       g.setCameraInfoTopic("/camera/color/camera_info");
       g.setRectify(true);
     }
 
     g.open(I);
     std::cout << "Image size: " << I.getWidth() << " " << I.getHeight() << std::endl;
-         bool firstRun = true;
-        bool init_done = false;
-        vpDot2 blob;
-        vpImagePoint germ;
+
 #ifdef VISP_HAVE_X11
     vpDisplayX d(I);
 #else
     std::cout << "No image viewer is available..." << std::endl;
 #endif
-   vpImage<unsigned char> greyImage;
-    while(1) {
-      g.acquire(I);
-      vpImageConvert::convert(I, greyImage);
-      vpDisplay::display(greyImage);
-      vpDisplay::displayText(greyImage, 20, 20, "A click to quit...", vpColor::red);
-      vpDisplay::flush(greyImage);
-        
 
+    while (1)
+    {
+      try
+      {
+        g.acquire(I);
+        vpDisplay::display(I);
+        if (!init_done)
+        {
+          std::cout << "click the blob" << std::endl;
+          vpDisplay::displayText(I, vpImagePoint(10, 10), "click the blob", vpColor::red);
+          if (vpDisplay::getClick(I, detectedPoints, false))
+          {
+            std::cout << "should not be her" << std::endl;
+            blobDetector.initTracking(I, detectedPoints);
+            init_done = true;
+          }
+        }
+        else
+        {
+          std::cout << "trackiing " << std::endl;
+          blobDetector.track(I);
+        }
+        vpDisplay::displayText(I, 20, 20, "A click to quit...", vpColor::red);
+        vpDisplay::flush(I);
+        if (vpDisplay::getClick(I, false))
+          break;
+      }
 
-            try{
-              if (!init_done) {
-            std::cout << "initdone  " << init_done << std::endl;
-            vpDisplay::displayText(greyImage, vpImagePoint(10, 10), "Click in the blob to initialize the tracker", vpColor::red);
-            vpDisplay::flush(greyImage);
-            while (!init_done ){
-            std::cout << "first run " << std::endl;
-            if (vpDisplay::getClick(greyImage, germ, false)) {
-               std::cout << "init done  " << std::endl;
-                blob.setGraphics(true);
-                blob.setGraphicsThickness(2);
-                blob.initTracking(greyImage, germ);
-                init_done = true;
-                }}}
-
-             else {
-                blob.track(greyImage,germ);
-                std::cout << "blob  " << std::endl;
-            }
-            }
-            catch (...){
-                init_done=false;
-            }
-
-
-
-      if (vpDisplay::getClick(I, false))
-        break;
+      catch (...)
+      {
+        init_done = false;
+      }
     }
   }
-  catch(vpException e) {
+  catch (vpException e)
+  {
     std::cout << "Catch an exception: " << e << std::endl;
   }
 }
